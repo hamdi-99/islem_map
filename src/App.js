e7b663ab9fd1fc5723003dfd8019112582c067b9
hamdi-99 <hamdiharaketi@insat.u-carthage.tn>
2022-09-29 11:48:19 +0100

    project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from 'mapbox-gl';
+import data from './assets/data.json'
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiaGFtZGloYXJha2V0aTk5IiwiYSI6ImNrbWNrZmF6NzBibTcydmxqamJ4Y282bGsifQ.fBIztamOU1ulVWk0F19aaw';
 
 function App() {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(9.716);
+  const [lat, setLat] = useState(35.283);
+  const [zoom, setZoom] = useState(6);
+  const markers = useRef(new Array(data.length).fill(null))
+  const pop = ({ el }) => {
+    return `<h4>${el['port_name']}</h4>`
+  }
+
+  useEffect(() => {
+    if (!map.current) {
+      map.current = new mapboxgl.Map({
+        container: mapContainer.current,
+        style: 'mapbox://styles/hamdiharaketi99/ckmckiqe138qz17n3fv8olhgx',
+        center: [lng, lat],
+        zoom: zoom
+      });
+    }
+
+    markers.current.forEach((marker, i) => {
+      if (!markers.current[i]) {
+        markers.current[i] = new mapboxgl.Marker({
+          color: "#FFFFFF",
+          draggable: true
+        }).setLngLat([data[i]['longitude'], data[i]['latitude']])
+          .setPopup(new mapboxgl.Popup().setHTML(pop({ 'el': data[i] })))
+          .addTo(map.current);
+      }
+    })
+
+    // initialize map only once
+
+
+
+
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <p>IPA Swixtboard</p>
+      <div className="container">
+        <div ref={mapContainer} className="map-container" />
+      </div>
+
+    </div >
   );
 }
 
